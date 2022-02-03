53522f9c075cb63f3a31bcd0cd55eb4e81849d35
Athenkosi Vinqi <nervadevelop@gmail.com>
2022-02-03 10:46:33 +0200

    add pages

---
+++ index.js
@@ -0,0 +1,23 @@
+//jshint esversion:6
+
+const express = require("express");
+const app = express();
+const posts = require('./posts')
+
+app.get('/posts', function(req, res)
+{
+res.json(posts);
+});
+
+
+app.get('/', function(req, res)
+{
+res.send('API is running');
+});
+
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server started on port 3000");
+  });
+  
---
+++ posts.js
@@ -0,0 +1,227 @@
+module.exports = [
+    {
+      "_id": "61fa5e9b949b1ca046f774e4",
+      "index": 0,
+      "guid": "4830d602-c6de-4eab-83e5-e17a96dc2a26",
+      "isActive": true,
+      "balance": "$3,804.00",
+      "picture": "http://placehold.it/32x32",
+      "age": 24,
+      "eyeColor": "brown",
+      "name": "Bryan Cooke",
+      "gender": "male",
+      "company": "PIGZART",
+      "email": "bryancooke@pigzart.com",
+      "phone": "+1 (894) 508-3329",
+      "address": "659 Leonard Street, Hoagland, Delaware, 8905",
+      "about": "Nulla nisi dolor consequat labore proident pariatur et nulla. Culpa fugiat non nostrud eu qui sint officia cupidatat sit sit cillum deserunt laboris. Cupidatat ipsum esse et commodo mollit esse exercitation ipsum incididunt. Adipisicing elit enim mollit sint et ea esse ea est anim. Mollit laboris qui esse do irure non consectetur magna quis culpa aute in voluptate.\r\n",
+      "registered": "2015-11-26T08:29:30 -02:00",
+      "latitude": 74.73626,
+      "longitude": 114.76915,
+      "tags": [
+        "sit",
+        "sit",
+        "nulla",
+        "pariatur",
+        "non",
+        "eiusmod",
+        "magna"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Kenya Murphy"
+        },
+        {
+          "id": 1,
+          "name": "Morrison Finley"
+        },
+        {
+          "id": 2,
+          "name": "Rivera Day"
+        }
+      ],
+      "greeting": "Hello, Bryan Cooke! You have 7 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "61fa5e9b79e8b8ec554b1297",
+      "index": 1,
+      "guid": "2ae9313c-c003-4cb5-bac0-055d72276c67",
+      "isActive": false,
+      "balance": "$2,965.20",
+      "picture": "http://placehold.it/32x32",
+      "age": 26,
+      "eyeColor": "blue",
+      "name": "Nita Carver",
+      "gender": "female",
+      "company": "ZILCH",
+      "email": "nitacarver@zilch.com",
+      "phone": "+1 (930) 565-2841",
+      "address": "746 Dahill Road, Wells, New Hampshire, 6768",
+      "about": "Sint laborum qui dolore nisi ipsum consectetur. Magna proident est et excepteur duis laboris nostrud ex irure officia nulla aute eu. Adipisicing esse occaecat laboris eu nostrud nostrud nulla exercitation magna. Eu officia ullamco ea cillum laboris ut laborum enim. Consequat do fugiat eu nulla elit laboris aute consequat fugiat eiusmod nulla sint. Dolor excepteur amet irure laboris nulla labore ad voluptate officia ipsum commodo deserunt anim deserunt. Incididunt veniam pariatur esse officia qui qui esse dolore.\r\n",
+      "registered": "2020-02-21T02:02:38 -02:00",
+      "latitude": 73.837598,
+      "longitude": -173.77581,
+      "tags": [
+        "duis",
+        "tempor",
+        "voluptate",
+        "eiusmod",
+        "dolor",
+        "aliqua",
+        "nisi"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Terri Simmons"
+        },
+        {
+          "id": 1,
+          "name": "Tonya Washington"
+        },
+        {
+          "id": 2,
+          "name": "Bertie Mathews"
+        }
+      ],
+      "greeting": "Hello, Nita Carver! You have 3 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "61fa5e9b730e46bccb7b6233",
+      "index": 2,
+      "guid": "45733952-f3a4-4b56-88d6-a1e8d81a02cd",
+      "isActive": true,
+      "balance": "$2,667.04",
+      "picture": "http://placehold.it/32x32",
+      "age": 23,
+      "eyeColor": "blue",
+      "name": "Norris Noel",
+      "gender": "male",
+      "company": "URBANSHEE",
+      "email": "norrisnoel@urbanshee.com",
+      "phone": "+1 (920) 591-3863",
+      "address": "568 Sullivan Place, Florence, Puerto Rico, 5516",
+      "about": "Adipisicing cillum anim eu ut nostrud deserunt aliquip ut ullamco cupidatat quis. Officia tempor commodo mollit aute commodo mollit occaecat voluptate est deserunt nostrud. Amet pariatur ut et ipsum duis adipisicing culpa cupidatat irure ex. Et in elit duis labore aute. Proident ipsum aliquip voluptate amet eu ipsum incididunt eiusmod ex labore pariatur. Magna aliquip cillum incididunt ad id ipsum exercitation minim proident ad elit id.\r\n",
+      "registered": "2015-08-15T06:25:04 -02:00",
+      "latitude": 14.207357,
+      "longitude": -26.83094,
+      "tags": [
+        "nulla",
+        "aliquip",
+        "enim",
+        "pariatur",
+        "est",
+        "do",
+        "cillum"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Gallagher Daugherty"
+        },
+        {
+          "id": 1,
+          "name": "Georgia Rose"
+        },
+        {
+          "id": 2,
+          "name": "Darcy Mccormick"
+        }
+      ],
+      "greeting": "Hello, Norris Noel! You have 5 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "61fa5e9b237adb11d234db80",
+      "index": 3,
+      "guid": "6e1f697f-f403-4c35-8667-27ff41f8abd3",
+      "isActive": false,
+      "balance": "$2,300.63",
+      "picture": "http://placehold.it/32x32",
+      "age": 21,
+      "eyeColor": "blue",
+      "name": "Marcie Mccoy",
+      "gender": "female",
+      "company": "PROSURE",
+      "email": "marciemccoy@prosure.com",
+      "phone": "+1 (946) 411-2940",
+      "address": "466 Dearborn Court, Reno, Wyoming, 177",
+      "about": "Quis magna sit ad fugiat. Ea aute consequat reprehenderit qui. Pariatur exercitation occaecat consequat excepteur sit consectetur non dolore mollit. Nulla ut incididunt ut consequat ullamco ipsum veniam occaecat. Irure irure ullamco magna voluptate occaecat commodo cillum excepteur id exercitation culpa ipsum.\r\n",
+      "registered": "2018-09-27T09:15:56 -02:00",
+      "latitude": 15.936388,
+      "longitude": 80.916012,
+      "tags": [
+        "dolore",
+        "et",
+        "et",
+        "adipisicing",
+        "do",
+        "veniam",
+        "nostrud"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Miranda Roberts"
+        },
+        {
+          "id": 1,
+          "name": "Bright Ross"
+        },
+        {
+          "id": 2,
+          "name": "Levine Britt"
+        }
+      ],
+      "greeting": "Hello, Marcie Mccoy! You have 1 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "61fa5e9b225885da83258f19",
+      "index": 4,
+      "guid": "46b749d7-ffb2-4710-8275-8c57d01baabc",
+      "isActive": false,
+      "balance": "$2,378.50",
+      "picture": "http://placehold.it/32x32",
+      "age": 20,
+      "eyeColor": "green",
+      "name": "Rachael Woods",
+      "gender": "female",
+      "company": "SLOGANAUT",
+      "email": "rachaelwoods@sloganaut.com",
+      "phone": "+1 (851) 462-3424",
+      "address": "373 Jodie Court, Alden, Massachusetts, 4137",
+      "about": "Mollit incididunt aliqua nostrud adipisicing. Aliquip cillum minim minim amet incididunt nulla sit ut nostrud esse. Occaecat enim eu ex cupidatat ad irure veniam pariatur in. Ad exercitation elit eiusmod excepteur. Minim aute et reprehenderit reprehenderit minim proident tempor aliquip esse est qui excepteur.\r\n",
+      "registered": "2014-03-30T10:42:52 -02:00",
+      "latitude": -47.28603,
+      "longitude": -5.452083,
+      "tags": [
+        "incididunt",
+        "mollit",
+        "eu",
+        "eiusmod",
+        "ad",
+        "duis",
+        "officia"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Flores Gamble"
+        },
+        {
+          "id": 1,
+          "name": "Holmes Mendoza"
+        },
+        {
+          "id": 2,
+          "name": "Kristie Cobb"
+        }
+      ],
+      "greeting": "Hello, Rachael Woods! You have 6 unread messages.",
+      "favoriteFruit": "banana"
+    }
+  ]
